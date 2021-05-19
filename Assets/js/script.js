3b4662f73c920da6c048d12d2a7d45fb2c7a499f
Jenn Greiner <jenn.greiner1@gmail.com>
2021-05-18 19:20:29 -0600

    initial push

---
+++ Assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("Linked!");
+
+const githubIcon = document.getElementById('github');
+const linkedInIcon = document.getElementById('linkedin');
+const emailIcon = document.getElementById('email');
+
+githubIcon.addEventListener('click', location.replace('https://github.com/jenngreiner'));
