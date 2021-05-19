a6977d13dbc731b6c422cc2a648803091b06164c
Jenn Greiner <jenn.greiner1@gmail.com>
2021-05-19 09:28:03 -0600

    footer & resume links

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,19 @@
 console.log("Linked!");
 
-const githubIcon = document.getElementById('github');
-const linkedInIcon = document.getElementById('linkedin');
-const emailIcon = document.getElementById('email');
+const fName = document.getElementById("first-name");
+const lName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const message = document.getElementById("email-body");
 
-githubIcon.addEventListener('click', location.replace('https://github.com/jenngreiner'));
+// function to check email is valid
+function isEmail(email) {
+  let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+// function to check fields are not empty
+function isNotEmpty(value) {
+  if (value == null || typeof value == "undefined") return false;
+  return value.length > 0;
+}
